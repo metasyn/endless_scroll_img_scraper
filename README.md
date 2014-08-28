8361c739b48bd2bb3b5cafc4dc3164057d3899cd
xander johnson <alexx.johnson@gmail.com>
2014-08-27 20:56:03 -0700

    fixes #5 and #3, add command line options, add license, contributors

--- README.md
+++ README.md
@@ -1,26 +1,37 @@
-About this program:
+Usage:
 ===================
 
 This is a program written in python that scrapes a URL page for images, specifically for pages that have a "endless scroll" feature such as tumblr.  
 
 To run just type this at the command line:
 python endless_scroll_img_scraper.py
-and you will be prompted to enter the URL you wish to scrape images from.
+and you will be prompted to enter the URL you wish to scrape images from. Be
+sure to put the URL in quotes.
 
 Optional command line arugments: 
 ================================
 
--- load_time: A float that is how many seconds the program waits for the website to load after each page down while scrolling to the end of the "endless scroll". Default is set to 0.5 seconds. This arg may need to be used to ensure all images are downloaded, dependent on your internet speed, the amount of data per load, etc. This arg can be used to quicken the scraping process, but if the load time is too small it may not scrape all images from the site. Example usage: python endless_scroll_img_scraper.py 0.75 
+-l OR -load_time: A float that is how many seconds the program waits for the website to load after each page down while scrolling to the end of the "endless scroll". Default is set to 0.5 seconds. This arg may need to be used to ensure all images are downloaded, dependent on your internet speed, the amount of data per load, etc. This arg can be used to quicken the scraping process, but if the load time is too small it may not scrape all images from the site. 
+
+Example usage: python endless_scroll_img_scraper.py -l 0.75 
+
+-f OR -file types: A space seperated list of file types that you want to
+scrape. The default is set to jpg and png, but you can set it to whatever you
+want.
+
+Example usage python endless_scroll_img_scraper.py -f "gif svg bmp"
+
 
 Requirements
 ============
 
 * BeautifulSoup
-* urllib2
-* urllib
 * selenium
+
+If you don't have them just simply
+    sudo pip install *package name*
 
 License
 =======
-
+WTFPL
 
--- endless_scroll_img_scraper.py
+++ endless_scroll_img_scraper.py
@@ -4,20 +4,24 @@ from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import sys
+import os
+import getopt
 from urllib import urlretrieve
 reload(sys)
 
-if(len(sys.argv) > 1):
-	load_time = float(sys.argv[1])
-else:
-	load_time = 0.5
-
-
-def imgGreb():
+def imgGreb(load_time, file_types):
 	# get the URL
+        print "=" * 70
+        print "Loadtime: ", load_time 
+        print "File Types: ", ' '.join(file_types) 
 	print "Enter the URL you wish to scrape.."
 	print 'Usage  - "http://uuuu----uuuu.tumblr.com/" <-- With the double quotes'
 	myurl = input("@> ")
+        print "=" * 70
+        # make custom folder
+        folder_name = str(myurl[7:])
+        os.mkdir(folder_name)
+        os.chdir(folder_name)
 	# open the URL in the browser
 	browser = webdriver.Firefox()
 	browser.get(myurl)
@@ -45,11 +49,49 @@ def imgGreb():
 			name = url.split('/')[-1] 
 			# detect if that is a type of pictures you want
 			type = name.split('.')[-1]
-			if type in ['jpg', 'png']:
+			if type in file_types:
 				# if so, retrieve the pictures
 				urlretrieve(url, name)
 		except:
 			pass
+        print "Finished Scraping"
+        print "=" * 70
+
+def main(argv):
+    # define default values
+    global load_time 
+    load_time = 0.5
+    global file_types
+    file_types = ["jpg", "png"]
+
+    # here we define what our options are
+    # we have an option flag, then an argument
+    try:
+        opts, args = getopt.getopt(argv, "l:f:", ["load_time", "file_types"])
+    except getopt.GetoptError:
+        # in case theres a mistake
+        usage()
+        sys.exit()
+    for opt, arg in opts:
+        if opt in ("-l", "-load_time"):
+            load_time = arg
+        elif opt in ("-f", "-file_types"):
+            file_types = arg.split()
+    
+    imgGreb(load_time, file_types)
+
+def usage():
+    # This function is called when options aren't specified correctly
+    print """\n
+    
+    Proper Option Usage:
+
+    -f OR -file_types: file type names seperated by a space, surrounded by
+    quotes, e.g. "jpg png img" or "png svg gif"
+
+    -l OR -load_time: a number that will set desired load times for the page
+    """
 
 if __name__ == '__main__':
-	imgGreb()
+    main(sys.argv[1:])
+    # we exclude the first argv because its the script name
